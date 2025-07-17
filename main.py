b70a1a3733e63436ba4f4819e50e66460101c31b
Dell Alpha <Dell Alpha@DELL-ALPHA-01>
2025-07-17 14:55:16 +0700

    use PORT from env and host=0.0.0.0 for deploy

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ app.include_router(user_api.router)
 if __name__ == "__main__":
     import uvicorn
 
-    host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", 8000))
 
-    uvicorn.run("main:app", host=host, port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
